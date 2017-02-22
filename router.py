6d40fff97c530e4e2ca83154ede376101bb4d0e0
Lucas de Matos <ldmatos8@gmail.com>
2017-02-21 21:46:09 -0300

    criando db

--- router.py
+++ router.py
@@ -4,22 +4,57 @@ import datetime
 from core.controller import Controller
 from core import database
 
+import sqlite3
+
 from flask import Flask
 from flask import render_template
-from flask import Flask, request, redirect, jsonify, abort
+from flask import Flask, request, redirect, jsonify, abort, g
 from datetime import datetime,  date, timedelta
 
 #-------------------- CONFIG --------------------
 
-app = Flask(__name__)
+template_dir = os.path.abspath('static')
+app = Flask(__name__, template_folder=template_dir)
 
-app.config.update(
-	MAX_CONTENT_LENGTH = 1024 * 1024,
-	DEBUG = True
-	#,SQLALCHEMY_DATABASE_URI = 'sqlite:///app-name.db',
-	#SQLALCHEMY_TRACK_MODIFICATIONS = False,
-	#SQLALCHEMY_POOL_RECYCLE = 280
-)
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 #---------------------- MAIN --------------------
 
@@ -33,7 +68,18 @@ controller = Controller()
 
 @app.route('/')
 def root():
-    return redirect('/site/')
+    db = get_db()
+    cur = db.execute('select name, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
+
+@app.route('/item')
+    db = get_db()
+    db.execute('insert into entries (name, text) values (?, ?)',
+                 [request.form['name'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return render_template('index.html', entries=entries)
 
 @app.route('/site/')
 @app.route('/site/<path:url>')
