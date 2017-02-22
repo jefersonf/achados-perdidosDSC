f7715ce5bb8c5c3660ca1cd04a98e801b5a51c89
Leticia Maia <leticiamt@hotmail.com>
2017-02-22 18:17:08 -0300

    colocando portlets

--- static/app/views/home/controller.js
+++ static/app/views/home/controller.js
@@ -1,3 +1,3 @@
 app.controller('homeController', function($scope, $state, $http) {
-  $scope.items = [{title: "hello"}];
+  $scope.items = [{title: "hello", description: "I am a nice object!"}, {title: "hello2", description: "I am a nice object!"}];
 });
