246523f06d166b9391ecbe18056a56b2df38f68a
Leticia Maia <leticiamt@hotmail.com>
2017-02-27 21:25:22 -0300

    Adicionando limite de caracteres nos campos e Categoria default

--- static/app/views/home/controller.js
+++ static/app/views/home/controller.js
@@ -1,6 +1,5 @@
 app.controller('homeController', function($scope, $state, $http) {
   $scope.items = [];
-  $scope.addItemForm = {};
   $http.get('/item', {params:{}}).
 		then(function(response) {
 				$scope.items = response.data.item;
