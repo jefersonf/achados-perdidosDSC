f8f5e900badf12d45bbdddd3a2b9c52d77803892
Leticia Maia <leticiamt@hotmail.com>
2017-03-21 22:51:40 -0300

    Adicionando coluna e resolvidos

--- static/app/views/home/controller.js
+++ static/app/views/home/controller.js
@@ -1,5 +1,6 @@
 app.controller('homeController', function($scope, $state, $http) {
   $scope.items = [];
+  $scope.showResolved = false;
   $http.get('/item', {params:{}}).
 		then(function(response) {
 				$scope.items = response.data.item;
